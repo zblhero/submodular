1860e463c1ee29a38793b50f4b6709204afcb79f
zblhero <zblhero@gmail.com>
2015-05-20 22:09:54 +0800

    client

--- submodular.py
+++ submodular.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello_world():
-    return 'Hello World!'
+from weibo import Client
+
+APP_KEY = '2975032421'
+APP_SECRET = '25e39c3acf0310a2824e94b619773097'
+CALLBACK_URL = 'http://apps.weibo.com/submodular'
 
 @app.route('/login')
 def login():
@@ -15,7 +17,13 @@ def login():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    c = Client(APP_KEY, APP_SECRET, CALLBACK_URL)
+    c.set_code('3e6c11673cebce92d25b55ce3883553b')
+    
+    token = c.token
+    user = c.get('users/show', uid=2703275934)
+    
+    return user.location
 
 
 if __name__ == "__main__":
